ce1e5234320d2d569c632a53a191dd6203104253
nickolss-school <nickolasmaraujo@gmail.com>
2025-09-19 19:35:19 -0300

    feat: added list products screen

--- app/src/main/java/com/nickolss/aula_tapweb/MainActivity.kt
+++ app/src/main/java/com/nickolss/aula_tapweb/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +21,7 @@ import androidx.navigation.navArgument
 import com.nickolss.aula_tapweb.ui.theme.AulatapwebTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,7 +70,9 @@ class MainActivity : ComponentActivity() {
                                 }
                             )
                         }
-
+                        composable("listarProdutos") {
+                            ListarProdutosScreen(onBack = { navController.popBackStack() })
+                        }
                     }
                 }
             }
